e604113a57a582cf48cdc4d4b76263a51ce9672e
cmkennedy20 <cmkennedy20@gmail.com>
2023-09-09 12:07:03 -0400

    Change file

--- src/s3/terraform/provider.tf
+++ src/s3/terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-  access_key = var.TF_VAR_access_key
-  secret_key = var.TF_VAR_private_key
+  region                      = "us-east-1"
+  access_key                  = "fake"
+  secret_key                  = "fake"
 }
