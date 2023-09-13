d9a66deb513dc9c12529ec6db2f4af20920eb0e2
cmkennedy20 <cmkennedy20@gmail.com>
2023-09-12 21:04:26 -0400

    Pass it as a TF var

--- src/s3/terraform/provider.tf
+++ src/s3/terraform/provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "fake"
-  secret_key                  = "fake"
+  access_key                  = var.TF_VAR_access_key
+  secret_key                  = var.TF_VAR_private_key
 }
